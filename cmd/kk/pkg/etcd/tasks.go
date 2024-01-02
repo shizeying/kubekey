26c7467b5898729fac9379f9ec2bd86bc39f8c08
shizeying <w741069229@163.com>
2024-01-02 20:21:38 +0800

    支持 arm64

--- cmd/kk/pkg/etcd/tasks.go
+++ cmd/kk/pkg/etcd/tasks.go
@@ -312,6 +312,7 @@ func refreshConfig(KubeConf *common.KubeConf, runtime connector.Runtime, endpoin
 			"MaxWals":             KubeConf.Cluster.Etcd.MaxWals,
 			"ElectionTimeout":     KubeConf.Cluster.Etcd.ElectionTimeout,
 			"HeartbeatInterval":   KubeConf.Cluster.Etcd.HeartbeatInterval,
+			"Address":             host.GetAddress(),
 		},
 	}
 
